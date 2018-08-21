fd6576af45b23710a47ce82d0083808ad31bc160
Maikel Angueira <michael.angueira@gmail.com>
2018-08-21 13:26:38 -0400

    Create initial database tables.

---
+++ Systems.Appollo.Shooes.DB/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Systems.Appollo.Shooes.DB
+{
+    public class Class1
+    {
+    }
+}
