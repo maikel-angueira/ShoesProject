0fa93c01987ce32150e5bf788a93775762108622
Maikel Angueira <michael.angueira@gmail.com>
2018-08-22 18:33:58 -0400

    Modify show messages.

--- Systems.Appollo.Shoes.Client.WinForm/Utils/Messages.cs
+++ Systems.Appollo.Shoes.Client.WinForm/Utils/Messages.cs
@@ -14,7 +14,7 @@ namespace Systems.Appollo.Shoes.Client.WinForm.Utils
         public const string COLOR_NAME_EXIST = "El color ya existe en la base de datos.";
         public const string COLOR_INSERT_SUCESS = "Nuevo color insertado correctamente {0}";
         public const string COLOR_UPDATED_SUCESS = "El color fue actualizado correctamente";
-        public const string COLOR_DELETED_SUCESS = "Color eliminado correctamente {0}";
-        public const string DO_YOU_WANT_TO_DELETED = "¿Estas seguro que quieres borrar el color seleccionado {0}?";
+        public const string COLOR_DELETED_SUCESS = "Color eliminado correctamente: '{0}'";
+        public const string DO_YOU_WANT_TO_DELETED = "¿Estas seguro que quieres borrar el color seleccionado: '{0}'?";
     }
 }
