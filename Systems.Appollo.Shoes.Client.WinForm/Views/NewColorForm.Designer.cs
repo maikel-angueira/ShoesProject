128c039c06147867d6e144f45b4d4f4123a5821c
Maikel Angueira <michael.angueira@gmail.com>
2018-08-22 12:26:22 -0400

    Modify color form.

--- Systems.Appollo.Shoes.Client.WinForm/Views/NewColorForm.Designer.cs
+++ Systems.Appollo.Shoes.Client.WinForm/Views/NewColorForm.Designer.cs
@@ -33,13 +33,12 @@
             this.newButton = new System.Windows.Forms.Button();
             this.closeButton = new System.Windows.Forms.Button();
             this.linkLabel1 = new System.Windows.Forms.LinkLabel();
-            this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(23, 36);
+            this.label1.Location = new System.Drawing.Point(21, 21);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(34, 13);
             this.label1.TabIndex = 0;
@@ -47,14 +46,14 @@
             // 
             // colorTextBox
             // 
-            this.colorTextBox.Location = new System.Drawing.Point(26, 55);
+            this.colorTextBox.Location = new System.Drawing.Point(24, 40);
             this.colorTextBox.Name = "colorTextBox";
             this.colorTextBox.Size = new System.Drawing.Size(360, 20);
             this.colorTextBox.TabIndex = 1;
             // 
             // newButton
             // 
-            this.newButton.Location = new System.Drawing.Point(242, 133);
+            this.newButton.Location = new System.Drawing.Point(240, 122);
             this.newButton.Name = "newButton";
             this.newButton.Size = new System.Drawing.Size(68, 20);
             this.newButton.TabIndex = 2;
@@ -63,7 +62,7 @@
             // 
             // closeButton
             // 
-            this.closeButton.Location = new System.Drawing.Point(316, 133);
+            this.closeButton.Location = new System.Drawing.Point(314, 122);
             this.closeButton.Name = "closeButton";
             this.closeButton.Size = new System.Drawing.Size(68, 20);
             this.closeButton.TabIndex = 3;
@@ -74,33 +73,23 @@
             // linkLabel1
             // 
             this.linkLabel1.AutoSize = true;
-            this.linkLabel1.Location = new System.Drawing.Point(156, 137);
+            this.linkLabel1.Location = new System.Drawing.Point(154, 126);
             this.linkLabel1.Name = "linkLabel1";
             this.linkLabel1.Size = new System.Drawing.Size(61, 13);
             this.linkLabel1.TabIndex = 4;
             this.linkLabel1.TabStop = true;
             this.linkLabel1.Text = "Ver Colores";
             // 
-            // groupBox1
-            // 
-            this.groupBox1.Location = new System.Drawing.Point(3, 5);
-            this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(392, 161);
-            this.groupBox1.TabIndex = 5;
-            this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "Nuevo Color:";
-            // 
             // NewColorForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(398, 175);
+            this.ClientSize = new System.Drawing.Size(390, 158);
             this.Controls.Add(this.linkLabel1);
             this.Controls.Add(this.closeButton);
             this.Controls.Add(this.newButton);
             this.Controls.Add(this.colorTextBox);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.groupBox1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.MaximizeBox = false;
             this.MinimizeBox = false;
@@ -119,6 +108,5 @@
         private System.Windows.Forms.Button newButton;
         private System.Windows.Forms.Button closeButton;
         private System.Windows.Forms.LinkLabel linkLabel1;
-        private System.Windows.Forms.GroupBox groupBox1;
     }
 }
